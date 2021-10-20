8d3bfb0961df3641ac9c52f4d87450861858148f
Sgonchar89 <sgon4ar@bk.ru>
2021-10-19 22:35:38 -0700

    19

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        # 'NAME': os.environ.get('DB_NAME'),
+        'NAME': os.environ.dict['DB_NAME'],
+        #'USER': os.environ.get('POSTGRES_USER'),
+        'USER': os.environ.dict['POSTGRES_USER'],
+        #'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'PASSWORD': os.environ.dict['POSTGRES_PASSWORD'],
+        #'HOST': os.environ.get('DB_HOST'),
+        'HOST': os.environ.dict['DB_HOST'],
+        #'PORT': os.environ.get('DB_PORT'),
+        'PORT': os.environ.dict['DB_PORT'],
     }
 }
 
